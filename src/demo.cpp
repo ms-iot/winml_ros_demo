68b81e8d9b0e7c3137d752d7b56cccec695bb8fd
Stuart Schaefer <stuart@theschaefers.com>
2018-09-19 13:35:18 -0700

    Add ML Tracking

--- src/demo.cpp
+++ src/demo.cpp
@@ -18,11 +18,14 @@ float targetYaw = 0.0f;
 bool haveLidar = false;
 std::vector<float> lidarRanges;
 const float kFOV = 70.0f;  // move to param
-const float kWidth = 410;
-const float kMinDistance = 0.1f;
-const float kMaxDistance = 1.0f;
+const float kCameraPixelWidth = 416.0f;
+const float kDegreePerCameraIndex = kFOV / kCameraPixelWidth;
+const float kMinDistance = 1.0f;
+const float kMaxDistance = 3.0f;
 
-bool useLidarOnly = true;
+ros::Time g_lastSeen;
+bool useLidarOnly = false;
+ros::Duration g_lastSeenTimeout(2);
 
 // Lidar data starts at Zero straight ahead
 void scanCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
@@ -34,22 +37,22 @@ void scanCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
 		float degreePerIndex = 360.0f / lidarRanges.size();
 
 		// Zero is forward. Start from FOV loop around
-		int swingIndicies = (int)(kFOV / degreePerIndex);
-		int startingIndex = -swingIndicies / 2;
+		int swingIndices = (int)(kFOV / degreePerIndex);
+		int startingIndex = -swingIndices / 2;
 
 		targetYaw = 0.0f;
 
 		float closestMarkDistance = FLT_MAX;
-		for (int i = startingIndex; i < swingIndicies; i++)
+		for (int degreeIndex = startingIndex; degreeIndex < swingIndices; degreeIndex++)
 		{
-			size_t index = i < 0 ? lidarRanges.size() + i : i;
+			size_t index = degreeIndex < 0 ? lidarRanges.size() + degreeIndex : degreeIndex;
 			float distance = lidarRanges[index];
 			if (distance > kMinDistance &&
 				distance < kMaxDistance &&
 				distance < closestMarkDistance)
 			{
 				closestMarkDistance = distance;
-				targetYaw = (float)angles::from_degrees(i * degreePerIndex);
+				targetYaw = (float)angles::from_degrees(degreeIndex * degreePerIndex);
 			}
 		}
 	}
@@ -61,31 +64,36 @@ void markerCallback(const visualization_msgs::MarkerArray::ConstPtr& msg)
 		return;
 
 	float closestMarkerDistance = FLT_MAX;
+    float degreePerLidarIndex = 360.0f / lidarRanges.size();
+
+    targetYaw = 0.0f;
 
 	for (auto &marker : msg->markers)
 	{
-        float degreePerIndex = 360.0f / lidarRanges.size();
+        // Map the centroid position reported by the marker to Lidar index
+        float percentInImage = marker.pose.position.x / kCameraPixelWidth;
 
-        // Zero is forward. Start from FOV loop around
-        int swingIndicies = (int)(kFOV / degreePerIndex);
-        int startingIndex = -swingIndicies / 2;
-        float degreesInImage = kFOV * (marker.pose.position.x / kWidth);
-        int indexForImage = (int)(degreesInImage / degreePerIndex);
-        size_t index = indexForImage < 0 ? lidarRanges.size() + i : i;
+        // Zero is forward for the Lidar. Camera is centered around zero.
+        int swingIndices = (int)(kFOV / degreePerLidarIndex);
+        int degreeIndex = (int)(percentInImage * swingIndices - swingIndices / 2);
 
-        float distanceForMarker = lidarRanges[index];
+        size_t index = degreeIndex < 0 ? lidarRanges.size() + degreeIndex : degreeIndex;
+        float distance = lidarRanges[index];
         
         if (distance > kMinDistance &&
             distance < kMaxDistance &&
             distance < closestMarkerDistance)
         {
-            closestMarkerDistance = distanceForMarker;
-			targetYaw = (float)angles::from_degrees(lidarDegree);
-		}
+            closestMarkerDistance = distance;
+			targetYaw = (float)angles::from_degrees(- degreeIndex * degreePerLidarIndex);
+            g_lastSeen = ros::Time::now();
+        }
 	}
+
+    ROS_INFO("Turning Towards %f", targetYaw);
 }
 
-const float kTolerance = 0.10f;
+const float kTolerance = 0.05f;
 const float kAngleScale = 0.3f;
 
 int main(int argc, char** argv)
@@ -97,19 +105,21 @@ int main(int argc, char** argv)
 	ros::Subscriber markerSub = n.subscribe("tracked_objects", 500, markerCallback);
     ros::Publisher velocityPub = n.advertise<geometry_msgs::Twist>("cmd_vel", 10);
 
+    g_lastSeen = ros::Time::now();
+
 	ros::Rate rate(30);
     while (ros::ok())
     {
 		geometry_msgs::Twist vel_msg;
-
-		if (fabs(targetYaw) > kTolerance)
+        if (ros::Time::now() - g_lastSeen > g_lastSeenTimeout ||
+            fabs(targetYaw) < kTolerance)
+        {
+            vel_msg.angular.z = 0.0f;
+            velocityPub.publish(vel_msg);
+        }
+        else
 		{
 			vel_msg.angular.z = kAngleScale * targetYaw;
-			velocityPub.publish(vel_msg);
-		}
-		else
-		{
-			vel_msg.angular.z = 0.0f;
 			velocityPub.publish(vel_msg);
 		}
 
